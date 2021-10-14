1d8e408b50cbd29cf19a0f148c1ad275f090e4dd
beardemo11 <a22031227@gmail.com>
2021-10-14 19:54:35 +0800

    前端用Vue呈現

--- HakkaFood/Controllers/HomeController.cs
+++ HakkaFood/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using HakkaFood.Daos;
@@ -14,11 +16,7 @@ namespace HakkaFood.Controllers
         private HakkaFoodDataBaseEntities db = new HakkaFoodDataBaseEntities();
         public ActionResult Index()
         {
-            HakkaFoodDao hakkaFoodDao = new HakkaFoodDao();
-
-            List<HakkaFoodJsonModel> hakkaFoodJsonModels = hakkaFoodDao.GetAll().Take(5).ToList();
-
-            return View(hakkaFoodJsonModels);
+            return View();
         }
 
 
@@ -29,7 +27,7 @@ namespace HakkaFood.Controllers
             HakkaFoodDao hakkaFoodDao = new HakkaFoodDao();
 
             List<HakkaFoodJsonModel> hakkaFoodJsonModels = hakkaFoodDao.GetAll().Where(x => x.dishes_name == id).ToList();
-            string strResult = "false";
+            string strResult = "";
             try
             {
                 foreach (var item in hakkaFoodJsonModels)
@@ -44,7 +42,7 @@ namespace HakkaFood.Controllers
                     data.Url = item.Url;
                     db.HakkaFoodData.Add(data);
                     db.SaveChanges();
-                    strResult = "true";
+                    strResult = data.DishesName + "存入資料庫!";
                 }
                 return strResult;
 
@@ -57,7 +55,19 @@ namespace HakkaFood.Controllers
 
         }
 
+        public string GetInfo(string id)
+        {
+            const string url = "https://cloud.hakka.gov.tw/Pub/Opendata/DTST20171100016.json";
 
+            var webClient = new WebClient { Encoding = Encoding.UTF8 };
+
+            var response = webClient.DownloadString(url);
+
+            var hakkaFoodJsons = JsonConvert.DeserializeObject<List<HakkaFoodJsonModel>>(response).Where(x=>x.specialty_dishes.Length <50);
+
+            return JsonConvert.SerializeObject(hakkaFoodJsons);
+
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- HakkaFood/Models/HakkaFoodJsonModel.cs
+++ HakkaFood/Models/HakkaFoodJsonModel.cs
@@ -8,13 +8,25 @@ namespace HakkaFood.Models
 {
     public class HakkaFoodJsonModel
     {
-        
+        [Display(Name = "類別")]
         public string kind { get; set; }
+
+        [Display(Name = "分類")]
         public string classification { get; set; }
+
+        [Display(Name = "菜色名稱")]
         public string dishes_name { get; set; }
+
+        [Display(Name = "創作者")]
         public string creative { get; set; }
+
+        [Display(Name = "菜餚特色")]
         public string specialty_dishes { get; set; }
+
+        [Display(Name = "作法")]
         public string Practic { get; set; }
+
+        [Display(Name = "網址")]
         public string Url { get; set; }
     }
 }
